29b86af7cbdc436aa188d413b9f78bd202e5138a
brendacardenas <brenda_gcc@hotmail.com>
2019-12-02 08:37:35 -0600

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { HelloComponent } from './hello.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CardsComponent } from './cards/cards.component';
+import { FilmDetailsComponent } from './film-details/film-details.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NavMenuComponent, HomeComponent, CardsComponent ],
+  declarations: [ AppComponent, HelloComponent, NavMenuComponent, HomeComponent, CardsComponent, FilmDetailsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
